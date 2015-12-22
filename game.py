9a75aa6f7a50dcfd36b8d9f4eb8cc168738e0800
vanik <vanik_888@mail.ru>
2015-12-23 02:06:52 +0300

    astar works

--- game.py
+++ game.py
@@ -4,6 +4,7 @@
 import os
 import pygame
 
+from operator import itemgetter
 from itertools import product
 from pygame import *
 from blocks import *
@@ -20,19 +21,44 @@ DISPLAY = (WIN_WIDTH, WIN_HEIGHT)
 BACKGROUND_COLOR = "#004000"
 
 
-def make_graph(width, height):
+def make_graph(width, height, barriers):
     nodes = [[AStarGridNode(x, y) for y in range(height)] for x in range(width)]
     graph = {}
     for x, y in product(range(width), range(height)):
-        node = nodes[x][y]
-        graph[node] = []
-        for i, j in product([-1, 0, 1], [-1, 0, 1]):
-            if not (0 <= x + i < width):
-                continue
-            if not (0 <= y + j < height):
-                continue
-            graph[nodes[x][y]].append(nodes[x+i][y+j])
+        if (x, y) not in barriers:
+            node = nodes[x][y]
+            graph[node] = []
+            for i, j in product([-1, 0, 1], [-1, 0, 1]):
+                if not (0 <= x + i < width):
+                    continue
+                if not (0 <= y + j < height):
+                    continue
+                if not (x+i,y+j) in barriers:
+                    graph[nodes[x][y]].append(nodes[x+i][y+j])
     return graph, nodes
+
+
+def get_static_barriers(level):
+    x = 0
+    y = 0
+    barriers = []
+    for row in level:
+        for col in row:
+            if col == "-":
+                barriers.append((x, y))
+            x += 1
+        y += 1
+        x = 0
+
+    barriers_array = []
+    for i in range( max([k[0] for k in barriers]) +1):
+        barriers_row = []
+        for item in barriers:
+            if item[0] == i:
+                barriers_row.append(item)
+        barriers_array.append(barriers_row)
+    return barriers
+
 
 
 def main():
@@ -43,23 +69,13 @@ def main():
                                           # будем использовать как фон
     bg.fill(Color(BACKGROUND_COLOR))      # Заливаем поверхность сплошным цветом
 
+
+
+
     timer = pygame.time.Clock()
-    hero = Robot(CELL_SIZE*1, CELL_SIZE*1)
-    left = False
-    right = False
-    up = False
-    down = False
-
-    graph, nodes = make_graph(CART_HEIGHT, CART_WIDTH)
-    paths = AStarGrid(graph)
-    start, end = nodes[2][2], nodes[4][4]
-    path = paths.search(start, end)
-    hero.set_path(path)
-
-
     entities = pygame.sprite.Group() # Все объекты
     platforms = [] # то, во что мы будем врезаться или опираться
-    entities.add(hero)
+
 
     level = [
            "-------------------------",
@@ -83,8 +99,20 @@ def main():
            "-                       -",
            "-------------------------"]
 
+    barriers = get_static_barriers(level)
+    START_CELL_X = 1
+    START_CELL_Y = 1
+    hero = Robot(CELL_SIZE*START_CELL_X, CELL_SIZE*START_CELL_Y)
+    entities.add(hero)
+
+    graph, nodes = make_graph(CART_WIDTH, CART_HEIGHT, barriers)
+    paths = AStarGrid(graph)
+    start, end = nodes[START_CELL_X][START_CELL_Y], nodes[1][15]
+    path = paths.search(start, end)
+    hero.set_path(path)
+
     while 1: # Основной цикл программы
-        timer.tick(60)
+        timer.tick(2000)
         for e in pygame.event.get(): # Обрабатываем события
             if e.type == QUIT:
                 raise SystemExit, "QUIT"
@@ -108,18 +136,10 @@ def main():
                 down = False
 
         screen.blit(bg, (0, 0))      # Каждую итерацию необходимо всё перерисовывать
-        x = 0
-        y = 0
 
-        for row in level:
-            for col in row:
-                if col == "-":
-                    pf = Platform(x, y)
-                    entities.add(pf)
-                    platforms.append(pf)
-                x += PLATFORM_WIDTH
-            y += PLATFORM_HEIGHT
-            x = 0
+        for b in barriers:
+            pf = Platform(b[0]*CELL_SIZE, b[1]*CELL_SIZE)
+            entities.add(pf)
 
         # hero.update(left, right, up, down, platforms) # передвижение
         hero.move()
